77d848361502b33b50e33e3ad6144cd2dad40903
Anastasija Mezale <anastasija.mezale@diatomenterprises.com>
2023-05-16 17:27:41 +0300

    Setting up an HTTP request, exception handling and subscribing to an observable

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'pm-products',
@@ -8,11 +9,13 @@ import { ProductService } from "./product.service";
   styleUrls: ['./product-list.component.css'],
   providers: [ProductService]
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 2;
   showImage = false;
+  errorMessage = '';
+  sub!: Subscription;
   
   private _listFilter = ''
   get listFilter(): string {
@@ -40,8 +43,17 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.sub = this.productService.getProducts().subscribe({
+        next: products => {
+            this.products = products;
+            this.filteredProducts = this.products;
+        },
+        error: err => this.errorMessage = err
+    });
+  }
+
+  ngOnDestroy(): void {
+      this.sub.unsubscribe();
   }
 
   onRatingClicked(message: string): void {
